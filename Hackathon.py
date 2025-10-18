228a1b00ec1126814d7ed11c96b52ff795e647f2
ThomasGK42 <Thomjuniorgk42@gmail.com>
2025-10-18 11:48:00 +0900

    Add files via upload

---
+++ Hackathon.py
@@ -0,0 +1,39 @@
+
+dictFish = {"sashimi trout" : "サーモントラウト", "fish fillet mackerel" : "お魚切り身 さば", "white shrimp" : "ホワイトえび",
+            "squid fillet cut" : "いか切身カット", "seafood mix" : "シーフードミックス", "shelled clams " : "あさりむき身",
+            "salted sockeye salmon" : "お魚切り身 塩紅鮭", "red fish pickled in sake lees" : "漬魚", "boiled whitebait" : "釜揚げしらす",
+            "tarako" : "たらこ", "shiokara" : "いか塩辛", "wakame seaweed from Naruto waters" : "鳴門水域産わかめ",
+            "fatty mackerel teriyaki" : "九州産うなぎ蒲焼", "fragrant genuine wasabi" : "香る本わさび"}
+
+dictDairy = {"white eggs L" : "白たまご L", "delicious milk" : "おいしい牛乳", "nature megumi" : "ナチュレ 恵 megumi",
+             "Hokkaido pure fresh cream 35" : "北海道純生クリーム３５", "Hokkaido sour cream" : "北海道サワークリーム",
+             "organic soy milk, unsweetened" : "有機豆乳 無調整", "almond effect" : "アーモンド効果",
+             "Tropicana Whole Fruit 100% Orange" : "トロピカーナ まるごと果実感 100％ オレンジ", "Hokkaido butter" : "北海道バター",
+             "Hotel margarine" : "ホテルマーガリン", "Cream cheese" : "クリームチーズ"}
+
+dictFishUrl = {"sashimi trout" : "https://netsuper.rakuten.co.jp/seiyu/item/13269915000000/?l-id=category_ranking_item_03",
+               "fish fillet mackerel" : "https://netsuper.rakuten.co.jp/seiyu/item/134582404040562/?l-id=category_ranking_item_02",
+               "white shrimp" : "https://netsuper.rakuten.co.jp/seiyu/item/134969832214039/",
+               "squid fillet cut" : "https://netsuper.rakuten.co.jp/seiyu/item/134589791876989/",
+               "seafood mix" : "https://netsuper.rakuten.co.jp/seiyu/item/134940785147285/",
+               "shelled clams" : "https://netsuper.rakuten.co.jp/seiyu/item/134570078000098/",
+               "salted sockeye salmon" : "https://netsuper.rakuten.co.jp/seiyu/item/134582404040661/",
+               "red fish pickled in sake lees" : "https://netsuper.rakuten.co.jp/seiyu/item/134978496001367/",
+               "boiled whitebait" : "https://netsuper.rakuten.co.jp/seiyu/item/134984320710145/",
+               "tarako" : "https://netsuper.rakuten.co.jp/seiyu/item/134957971807045/",
+               "shiokara" : "https://netsuper.rakuten.co.jp/seiyu/item/134902584812771/",
+               "wakame seaweed from Naruto waters" : "https://netsuper.rakuten.co.jp/seiyu/item/134975041860851/",
+               "fatty mackerel teriyaki" : "https://netsuper.rakuten.co.jp/seiyu/item/134589849071502/",
+               "fragrant genuine wasabi" : "https://netsuper.rakuten.co.jp/seiyu/item/134967825190711/"}
+
+dictDairyUrl = {"white eggs L" : "https://netsuper.rakuten.co.jp/seiyu/item/4901995100309/",
+                "delicious milk" : "https://netsuper.rakuten.co.jp/seiyu/item/4902705126558/",
+                "nature megumi" : "https://netsuper.rakuten.co.jp/seiyu/item/49212877/",
+                "Hokkaido pure fresh cream 35" : "https://netsuper.rakuten.co.jp/seiyu/item/49854282/",
+                "Hokkaido sour cream" : "https://netsuper.rakuten.co.jp/seiyu/item/49853971/",
+                "organic soy milk, unsweetened" : "https://netsuper.rakuten.co.jp/seiyu/item/4902188122290/",
+                "almond effect" : "https://netsuper.rakuten.co.jp/seiyu/item/4971666410174/",
+                "Tropicana Whole Fruit 100% Orange" : "https://netsuper.rakuten.co.jp/seiyu/item/4909411091279/",
+                "Hokkaido butter" : "https://netsuper.rakuten.co.jp/seiyu/item/4903050155989/",
+                "Hotel margarine" : "https://netsuper.rakuten.co.jp/seiyu/item/4970208030238/",
+                "Cream cheese" : "https://netsuper.rakuten.co.jp/seiyu/item/4903050506378/"}
