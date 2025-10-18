2e9d859ed712fd93ca4f931c1430c7f09ced4937
ThomasGK42 <Thomjuniorgk42@gmail.com>
2025-10-18 12:01:17 +0900

    Update fishAndDairy.py

--- fishAndDairy.py
+++ fishAndDairy.py
@@ -13,20 +13,20 @@ dictFish = {"sashimi trout" : "サーモントラウト",
             "fatty mackerel teriyaki" : "九州産うなぎ蒲焼",
             "fragrant genuine wasabi" : "香る本わさび"}
 
-dictFishUrl = {"sashimi trout" : "https://netsuper.rakuten.co.jp/seiyu/item/13269915000000/?l-id=category_ranking_item_03",
-               "fish fillet mackerel" : "https://netsuper.rakuten.co.jp/seiyu/item/134582404040562/?l-id=category_ranking_item_02",
-               "white shrimp" : "https://netsuper.rakuten.co.jp/seiyu/item/134969832214039/",
-               "squid fillet cut" : "https://netsuper.rakuten.co.jp/seiyu/item/134589791876989/",
-               "seafood mix" : "https://netsuper.rakuten.co.jp/seiyu/item/134940785147285/",
-               "shelled clams" : "https://netsuper.rakuten.co.jp/seiyu/item/134570078000098/",
-               "salted sockeye salmon" : "https://netsuper.rakuten.co.jp/seiyu/item/134582404040661/",
-               "red fish pickled in sake lees" : "https://netsuper.rakuten.co.jp/seiyu/item/134978496001367/",
-               "boiled whitebait" : "https://netsuper.rakuten.co.jp/seiyu/item/134984320710145/",
-               "tarako" : "https://netsuper.rakuten.co.jp/seiyu/item/134957971807045/",
-               "shiokara" : "https://netsuper.rakuten.co.jp/seiyu/item/134902584812771/",
-               "wakame seaweed from Naruto waters" : "https://netsuper.rakuten.co.jp/seiyu/item/134975041860851/",
-               "fatty mackerel teriyaki" : "https://netsuper.rakuten.co.jp/seiyu/item/134589849071502/",
-               "fragrant genuine wasabi" : "https://netsuper.rakuten.co.jp/seiyu/item/134967825190711/"}
+dictFishUrl = {"サーモントラウト" : "https://netsuper.rakuten.co.jp/seiyu/item/13269915000000/?l-id=category_ranking_item_03",
+               "お魚切り身 さば" : "https://netsuper.rakuten.co.jp/seiyu/item/134582404040562/?l-id=category_ranking_item_02",
+               "ホワイトえび" : "https://netsuper.rakuten.co.jp/seiyu/item/134969832214039/",
+               "いか切身カット" : "https://netsuper.rakuten.co.jp/seiyu/item/134589791876989/",
+               "シーフードミックス" : "https://netsuper.rakuten.co.jp/seiyu/item/134940785147285/",
+               "あさりむき身" : "https://netsuper.rakuten.co.jp/seiyu/item/134570078000098/",
+               "お魚切り身 塩紅鮭" : "https://netsuper.rakuten.co.jp/seiyu/item/134582404040661/",
+               "漬魚" : "https://netsuper.rakuten.co.jp/seiyu/item/134978496001367/",
+               "釜揚げしらす" : "https://netsuper.rakuten.co.jp/seiyu/item/134984320710145/",
+               "いか塩辛" : "https://netsuper.rakuten.co.jp/seiyu/item/134957971807045/",
+               "いか塩辛" : "https://netsuper.rakuten.co.jp/seiyu/item/134902584812771/",
+               "鳴門水域産わかめ" : "https://netsuper.rakuten.co.jp/seiyu/item/134975041860851/",
+               "九州産うなぎ蒲焼" : "https://netsuper.rakuten.co.jp/seiyu/item/134589849071502/",
+               "香る本わさび" : "https://netsuper.rakuten.co.jp/seiyu/item/134967825190711/"}
 
 dictDairy = {"white eggs L" : "白たまご L",
              "delicious milk" : "おいしい牛乳",
@@ -50,4 +50,5 @@ dictDairyUrl = {"white eggs L" : "https://netsuper.rakuten.co.jp/seiyu/item/4901
                 "Hokkaido butter" : "https://netsuper.rakuten.co.jp/seiyu/item/4903050155989/",
                 "Hotel margarine" : "https://netsuper.rakuten.co.jp/seiyu/item/4970208030238/",
                 "Cream cheese" : "https://netsuper.rakuten.co.jp/seiyu/item/4903050506378/"}
+
 
